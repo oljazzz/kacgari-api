3492ad224e68f604e4024c1a6d80fdbfc24d0059
oljazzz <oljazzz362@gmail.com>
2017-10-21 23:21:35 +0600

    Collection name renamed to "word"

--- src/main/java/kz/kacgari/api/model/Word.java
+++ src/main/java/kz/kacgari/api/model/Word.java
@@ -11,7 +11,7 @@ import javax.validation.constraints.Size;
 
 @Data
 @NoArgsConstructor
-@Document(collection = "WORD")
+@Document(collection = "word")
 public class Word {
     @Id
     @Field("id")
